723cf57d68144023338cda2ca5f125223657ebd9
Edward Suárez <eddsuarez89@gmail.com>
2017-02-14 08:47:51 -0300

    first commit

---
+++ 3.4/set_mongodb_password.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USER=${MONGODB_USER:-"admin"}
+DATABASE=${MONGODB_DATABASE:-"admin"}
+PASS=${MONGODB_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MongoDB service startup"
+    sleep 5
+    mongo admin --eval "help" >/dev/null 2>&1
+    RET=$?
+done
+
+
+echo "=> Set authSchema version to 3"
+mongo admin --eval "db.system.users.remove({})"
+mongo admin --eval "db.system.version.remove({})"
+mongo admin --eval "db.system.version.insert({ '_id' : 'authSchema', 'currentVersion' : 3 })"
+
+echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
+
+if [ "$DATABASE" != "admin" ]; then
+    echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+    mongo admin -u $USER -p $PASS << EOF
+use $DATABASE
+db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'dbOwner',db:'$DATABASE'}]})
+EOF
+fi
+
+echo "=> Done"
+touch /data/db/.mongodb_password_set
+
+echo "Restart mongod"
+mongod --shutdown
+mongodb_cmd="mongod --storageEngine $STORAGE_ENGINE"
+cmd="$mongodb_cmd --master --auth"
+
+if [ "$JOURNALING" == "no" ]; then
+    cmd="$cmd --nojournal"
+fi
+
+if [ "$OPLOG_SIZE" != "" ]; then
+    cmd="$cmd --oplogSize $OPLOG_SIZE"
+fi
+
+$cmd
+
+echo "========================================================================"
+echo "You can now connect to this MongoDB server using:"
+echo ""
+echo "    mongo $DATABASE -u $USER -p $PASS --host <host> --port <port>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "========================================================================"
